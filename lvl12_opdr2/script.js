73e5fcc6aff48cd7dd22543358a8433b4933e217
roelwagner <70274035+roelwagner@users.noreply.github.com>
2020-09-21 14:12:54 +0200

    Add files via upload

---
+++ lvl12_opdr2/script.js
@@ -0,0 +1,13 @@
+console.log("Hallo Winc Academy studenten");
+// 3. Dit is een grote som
+/* 5. 1. console.log(1230941 * 1823794);
+2. console.log(637263 / 54);*/
+
+//4. comments worden overgeslagen door de console
+// singel line comment
+
+/*
+Multi
+Line
+Comment
+*/
